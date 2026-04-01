44c4d47dbcbf019c7d22c15d006a7228b504ee5d
Samuel Gallego <samuelgallego441@gmail.com>
2026-04-01 17:11:20 -0500

    footer y remover dependencias

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import ToolGrid from '@/components/ToolGrid';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <Hero />
         <ToolGrid />
       </div>
+      <Footer />
     </main>
   );
 }
